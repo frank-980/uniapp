40066c09078e5b070a93f00df28978e5f8416fbf
frank <1005253787@qq.com>
2020-06-13 11:38:54 +0800

    navBar

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,67 @@
+<template>
+	<view>
+		<view class="navBar" :style="{marginTop:statusBarHeight+'px'}">
+			<view class="content">
+				<view class="icon">
+					
+				</view>
+				<view class="text">
+					uni-app,vue
+				</view>
+			</view>
+		</view>
+		<!--导航栏高度 fixed定位 占位符-->
+		<view style="height:45px">
+			
+		</view>
+		<!--手机内置状态栏高度 占位符-->
+		<view :style="{height:statusBarHeight+'px'}">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				statusBarHeight:20
+			};
+		},
+		created(){
+			//H5 状态栏高度为0
+			const info = uni.getSystemInfoSync()
+			this.statusBarHeight=info.statusBarHeight
+		}
+	}
+</script>
+
+<style lang="scss">
+	.navBar{
+		height:45px;
+		width:100%;
+		padding:0 15px;
+		position: fixed;
+		top:0;
+		left:0;
+		background-color: $base-color;
+		box-sizing: border-box;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		.content{
+			display: flex;
+			align-items: center;
+			width: 100%;
+			background-color: white;
+			.icon{
+				width:30px;
+				height:30px;
+				
+			}
+			.text{
+				font-size: 12px;
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<navbar></navbar>
+		<view>
+			1
+		</view>
+		<view>
+			2
+		</view>
+		<view>
+			3
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list:[1,2,3,4,5,6]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
